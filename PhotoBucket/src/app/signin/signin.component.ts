c40ab06a59160a404c6cee3418a5ee98c4ee7926
Jonathan  Kurian <kurianj@rose-hulman.edu>
2017-07-24 23:54:49 -0700

    Rose-Hulman Login complete

--- PhotoBucket/src/app/signin/signin.component.ts
+++ PhotoBucket/src/app/signin/signin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from "angularfire2/auth";
+import { Router } from "@angular/router";
+import { environment } from "../../environments/environment";
+import 'rosefire';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private afAuth: AngularFireAuth, private router: Router) { }
   ngOnInit() {
+  }
+
+  signInWithRoseFire(): void{
+    console.log("TODO: Sign in with Rosefire");
+    Rosefire.signIn(environment.rosefireRegistryToken, (error, rfUser: RosefireUser) => {
+      if (error) {
+        // User not logged in!
+        console.error(error);
+        return;
+      }
+      console.log("rosefire is done. rfUser: ", rfUser);
+      this.afAuth.auth.signInWithCustomToken(rfUser.token).then((authState) => {
+        console.log("signing in is done. rfUser: ", authState);
+        this.router.navigate(["/"]);
+      });
+
+    });
   }
 
 }
--- PhotoBucket/src/environments/environment.prod.ts
+++ PhotoBucket/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  rosefireRegistryToken: "3f09cd9b-f812-4d6f-b43f-bcf59a295d58",
   firebaseconfig : {
     apiKey: "AIzaSyBTw7Q7Yy3n4RC3zHg_vNPhdg9GhMLAN94",
     authDomain: "kurianj-photo-bucket-93522.firebaseapp.com",
